41d2d462782500d5976ab9052c573da6cea410a4
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-04-04 01:11:18 +0530

    Update solver.py
    integrated with GUI.py

--- solver.py
+++ solver.py
@@ -1,50 +1,4 @@
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
-def print_board(bo):
-    for i in range(len(bo)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - - ")
-
-        for j in range(len(bo[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
-            if j == 8:
-                print(bo[i][j])
-            else:
-                print(str(bo[i][j]) + " ", end="")
-
-
-def find_empty(bo):
-    for i in range(len(bo)):
-        for j in range(len(bo[0])):
-            if bo[i][j] == 0:
-                return (i, j)  # row, col
-
-    return None
-
-board = [
-    [0,0,0,0,3,0,0,0,0],
-    [0,6,5,0,1,0,4,7,0],
-    [8,3,0,0,0,0,0,6,9],
-    [0,0,2,3,9,5,8,0,0],
-    [0,0,1,0,0,0,6,0,0],
-    [0,0,3,6,4,1,5,0,0],
-    [4,2,0,0,0,0,0,5,1],
-    [0,7,9,0,5,0,3,2,0],
-    [0,0,0,0,2,0,0,0,0]
-]
-
+# solver.py
 
 def solve(bo):
     find = find_empty(bo)
@@ -110,8 +64,3 @@ def find_empty(bo):
                 return (i, j)  # row, col
 
     return None
-
-print_board(board)
-solve(board)
-print("___________________")
-print_board(board)
